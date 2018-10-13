4e616899b046d855cfa28c0ab97b5d55cc0ebd74
Vishal Yarabandi <vishalgovindraddi.yarabandi@sjsu.edu>
2018-10-12 21:58:15 -0700

    Writing camera interface as a separate class

---
+++ camera.py
@@ -0,0 +1,84 @@
+import cv2
+import time
+
+class Camera:
+    """API for Camera interface on Nvidia Jetson TX2 using OpenCV"""
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.cap = None
+
+    def open_cam_rtsp(self, uri, latency):
+        gst_str = ("rtspsrc location={} latency={} ! rtph264depay ! h264parse ! omxh264dec ! "
+                   "nvvidconv ! video/x-raw, width=(int){}, height=(int){}, format=(string)BGRx ! "
+                   "videoconvert ! appsink").format(uri, latency, self.width, self.height)
+        self.cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+        return True
+
+    def open_cam_usb(self, dev):
+        # We want to set width and height here, otherwise we could just do:
+        #     return cv2.VideoCapture(dev)
+        gst_str = ("v4l2src device=/dev/video{} ! "
+                   "video/x-raw, width=(int){}, height=(int){}, format=(string)RGB ! "
+                   "videorate !"
+                   "video/x-raw,framerate=1/1 !"
+                   "videoconvert ! appsink").format(dev, self.width, self.height)
+        self.cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+        return True
+
+    def open_cam_onboard(self):
+        # On versions of L4T previous to L4T 28.1, flip-method=2
+        # Use Jetson onboard camera
+        gst_str = ("nvcamerasrc ! "
+                   "video/x-raw(memory:NVMM), width=(int)2592, height=(int)1458, format=(string)I420, framerate=(fraction)30/1 ! "
+                   "nvvidconv ! video/x-raw, width=(int){}, height=(int){}, format=(string)BGRx ! "
+                   "videoconvert ! appsink").format(self.width, self.height)
+        self.cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+        return True
+
+    def open_window(self, windowName, title="Camera Demo for Jetson TX2/TX1"):
+        cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(windowName, self.width, self.height)
+        cv2.moveWindow(windowName, 0, 0)
+        cv2.setWindowTitle(windowName, title)
+
+    def capture_image(self, fileName, showImage=False, windowName="Nvidia Jetson Tx2"):
+        ret, frame = self.cap.read()
+
+        if showImage:
+            show_image(frame, windowName)
+
+        cv2.imwrite(fileName, frame)
+
+    def show_image(self, frame, windowName):
+        showHelp = True
+        showFullScreen = False
+        helpText = "'Esc' to Quit, 'H' to Toggle Help, 'F' to Toggle Fullscreen"
+        font = cv2.FONT_HERSHEY_PLAIN
+        start_time = time.time()
+        while (time.time() - start_time < 1):
+            if cv2.getWindowProperty(windowName, 0) < 0: # Check to see if the user closed the window
+                break                                     # This will fail if the user closed the window; Nasties get printed to the console
+            if showHelp == True:
+                cv2.putText(frame, helpText, (11,20), font, 1.0, (32,32,32), 4, cv2.LINE_AA)
+                cv2.putText(frame, helpText, (10,20), font, 1.0, (240,240,240), 1, cv2.LINE_AA)
+            cv2.imshow(windowName, frame)
+            key = cv2.waitKey(10)
+            if key == 27: # ESC key: quit program
+                break
+            elif key == ord('H') or key == ord('h'): # toggle help message
+                showHelp = not showHelp
+            elif key == ord('F') or key == ord('f'): # toggle fullscreen
+                showFullScreen = not showFullScreen
+                
+            if showFullScreen:
+                cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+            else:
+               cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
+
+    def __del__(self):
+        if self.cap.isOpened():
+            cap.release()
+
+        cv2.destroyAllWindows()
